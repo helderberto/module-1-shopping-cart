23b3148472917fb1fa474d6017b807d551f19a45
Helder Burato Berto <helder.burato@gmail.com>
2021-08-30 20:04:34 +0100

    feat: add Cart methods structure

--- src/Cart.ts
+++ src/Cart.ts
@@ -1,1 +1,33 @@
-export class Cart {}
+interface Product {
+  title: string;
+  price: string;
+}
+
+interface Item {
+  quantity: number;
+  product: Product;
+}
+
+export class Cart {
+  items = [];
+
+  getTotal(): number {
+    return 0;
+  }
+
+  add(item: Item): void {
+    return;
+  }
+
+  remove(product: Product): void {
+    return;
+  }
+
+  summary(): void {
+    return;
+  }
+
+  checkout(): void {
+    return;
+  }
+}
