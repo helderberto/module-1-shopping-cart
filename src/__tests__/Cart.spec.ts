be1a726b378a3ac69fcc507c063a165e4b2705c9
Helder Burato Berto <helder.burato@gmail.com>
2021-09-18 20:36:11 +0100

    test: move tests that ensure total to getTotal() describe

--- src/__tests__/Cart.spec.ts
+++ src/__tests__/Cart.spec.ts
@@ -16,48 +16,50 @@ describe('Cart', () => {
     cart = new Cart();
   });
 
-  it('should return 0 when getTotal() is executed in a newly created instance', () => {
-    expect(cart.getTotal()).toBe(0);
-  });
-
-  it('should multiply quantity with price and receive the total amount', () => {
-    const item = {
-      product,
-      quantity: 2,
-    };
-
-    cart.add(item);
-
-    expect(cart.getTotal()).toBe(70776);
-  });
-
-  it('should ensure no more than one product exists at a time', () => {
-    cart.add({
-      product,
-      quantity: 2,
+  describe('getTotal()', () => {
+    it('should return 0 when getTotal() is executed in a newly created instance', () => {
+      expect(cart.getTotal()).toBe(0);
     });
 
-    cart.add({
-      product,
-      quantity: 1,
+    it('should multiply quantity with price and receive the total amount', () => {
+      const item = {
+        product,
+        quantity: 2,
+      };
+
+      cart.add(item);
+
+      expect(cart.getTotal()).toBe(70776);
     });
 
-    expect(cart.getTotal()).toBe(35388);
-  });
+    it('should ensure no more than one product exists at a time', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
 
-  it('should update total when a product get included and then removed from cart', () => {
-    cart.add({
-      product,
-      quantity: 2,
+      cart.add({
+        product,
+        quantity: 1,
+      });
+
+      expect(cart.getTotal()).toBe(35388);
     });
 
-    cart.add({
-      product: product2,
-      quantity: 1,
+    it('should update total when a product get included and then removed from cart', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 1,
+      });
+
+      cart.remove(product);
+
+      expect(cart.getTotal()).toBe(40000);
     });
-
-    cart.remove(product);
-
-    expect(cart.getTotal()).toBe(40000);
   });
 });
